012f489aaad75efb1fc827ffeb5c9407e2e3dd54
suchitra <suchitrathulabandi2003@gmail.com>
2025-12-01 15:14:36 +0530

    first commit

--- app.py
+++ app.py
@@ -1,14 +1,4 @@
-from dash import Dash, html, dcc
-import plotly.express as px
+import pandas as pd
 
-app = Dash(__name__)
-
-fig = px.line(x=[1, 2, 3], y=[10, 15, 13], title="Sample Line Chart")
-
-app.layout = html.Div([
-    html.H1("My First Dash App"),
-    dcc.Graph(figure=fig)
-])
-
-if __name__ == "__main__":
-    app.run(debug=True)
+df = pd.read_csv("car_price_prediction_.csv")
+print(df.head())
---
+++ test_data.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+df = pd.read_csv("car_price_prediction_.csv")
+print(df.head())
